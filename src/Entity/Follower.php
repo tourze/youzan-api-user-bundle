78ff86c07457334e844a0cf175fc902b726b6a0c
github-ci <you@example.com>
2025-06-18 11:25:09 +0800

    daily update

--- src/Entity/Follower.php
+++ src/Entity/Follower.php
@@ -4,12 +4,9 @@ namespace YouzanApiUserBundle\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
-use Tourze\DoctrineTimestampBundle\Attribute\CreateTimeColumn;
-use Tourze\DoctrineTimestampBundle\Attribute\UpdateTimeColumn;
+use Tourze\DoctrineTimestampBundle\Traits\TimestampableAware;
 use Tourze\EasyAdmin\Attribute\Column\ExportColumn;
 use Tourze\EasyAdmin\Attribute\Column\ListColumn;
-use Tourze\EasyAdmin\Attribute\Filter\Filterable;
 use YouzanApiBundle\Entity\Account;
 use YouzanApiUserBundle\Enum\GenderEnum;
 use YouzanApiUserBundle\Repository\FollowerRepository;
@@ -21,6 +18,7 @@ use YouzanApiUserBundle\Repository\FollowerRepository;
 #[ORM\Table(name: 'ims_youzan_user_follower', options: ['comment' => '有赞粉丝表'])]
 class Follower
 {
+    use TimestampableAware;
     #[ListColumn(order: -1)]
     #[ExportColumn]
     #[ORM\Id]
@@ -80,21 +78,6 @@ class Follower
     #[ORM\ManyToOne(targetEntity: LevelInfo::class)]
     #[ORM\JoinColumn(name: 'level_info_id', referencedColumnName: 'id', nullable: true)]
     private ?LevelInfo $levelInfo = null;
-
-    #[Filterable]
-    #[IndexColumn]
-    #[ListColumn(order: 98, sorter: true)]
-    #[ExportColumn]
-    #[CreateTimeColumn]
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
-    private ?\DateTimeInterface $createTime = null;
-
-    #[UpdateTimeColumn]
-    #[ListColumn(order: 99, sorter: true)]
-    #[Filterable]
-    #[ExportColumn]
-    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '更新时间'])]
-    private ?\DateTimeInterface $updateTime = null;
 
     public function getId(): ?int
     {
@@ -264,25 +247,4 @@ class Follower
     {
         $this->levelInfo = $levelInfo;
         return $this;
-    }
-
-    public function setCreateTime(?\DateTimeInterface $createdAt): void
-    {
-        $this->createTime = $createdAt;
-    }
-
-    public function getCreateTime(): ?\DateTimeInterface
-    {
-        return $this->createTime;
-    }
-
-    public function setUpdateTime(?\DateTimeInterface $updateTime): void
-    {
-        $this->updateTime = $updateTime;
-    }
-
-    public function getUpdateTime(): ?\DateTimeInterface
-    {
-        return $this->updateTime;
-    }
-}
+    }}
